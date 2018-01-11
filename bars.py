762c834ed31b9d4f1d06a8a3e792516afd372ace
John2013 <NaWasington@yandex.ru>
2018-01-11 17:07:06 +0300

    оптимизирована функция нахождения ближайшего бара добавлена проверка ввода пользователя

--- bars.py
+++ bars.py
@@ -2,6 +2,7 @@ import json
 import sys
 
 from haversine import get_distance
+from input import *
 
 
 def load_data(file_path: str):
@@ -11,37 +12,35 @@ def load_data(file_path: str):
 
 def get_biggest_bar(bars: dict):
     return max(
-        bars['features'],
+        bars,
         key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
 
 
 def get_smallest_bar(bars: dict):
     return min(
-        bars['features'],
+        bars,
         key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
 
 
 def get_closest_bar(bars: dict, users_longitude: float, users_latitude: float):
-    for bar in bars['features']:
-        bar['geometry']['distance'] = get_distance(
+    return min(
+        bars,
+        key=lambda bar: get_distance(
             bar['geometry']['coordinates'][0],
             bar['geometry']['coordinates'][1],
             users_longitude,
-            users_latitude
-        )
-
-    return min(bars['features'], key=lambda bar: bar['geometry']['distance'])
+            users_latitude))
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 1 and isfile(sys.argv[1]):
         file_path = sys.argv[1]
     else:
-        file_path = input("Введите путь до файла с данными о барах: ")
+        file_path = input_file("Введите путь до файла с данными о барах: ")
 
-    bars = load_data(file_path)
-    longitude = float(input("Введите долготу: "))
-    latitude = float(input("широту: "))
+    bars = load_data(file_path)['features']
+    longitude = input_float("Введите долготу: ")
+    latitude = input_float("широту: ")
 
     biggest_bar = get_biggest_bar(bars)
     smallest_bar = get_smallest_bar(bars)
