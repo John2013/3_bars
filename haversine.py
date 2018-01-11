bd7e324219ac29b90ca332ee8fd4cc045564d5e3
John2013 <NaWasington@yandex.ru>
2018-01-11 17:04:57 +0300

    убрана глобальная переменная

--- haversine.py
+++ haversine.py
@@ -1,20 +1,20 @@
 from math import radians, sin, cos, asin, sqrt
 
-EARTH_RADIUS = 6367
-
 
 def get_distance(
-        latitude1: float,
-        longitude1: float,
-        latitude2: float,
-        longitude2: float) -> float:
-    longitude1, latitude1, longitude2, latitude2 = \
-        map(radians, (longitude1, latitude1, longitude2, latitude2))
-
+    latitude1: float,
+    longitude1: float,
+    latitude2: float,
+    longitude2: float
+) -> float:
+    longitude1, latitude1, longitude2, latitude2 = map(
+        radians, (longitude1, latitude1, longitude2, latitude2)
+    )
+    earth_radius = 6367
     difference_longitude = longitude2 - longitude1
     difference_latitude = latitude2 - latitude1
     a = sin(difference_latitude / 2) ** 2 + cos(latitude1) * cos(latitude2) * \
         sin(difference_longitude / 2) ** 2
     c = 2 * asin(sqrt(a))
-    distance = EARTH_RADIUS * c
+    distance = earth_radius * c
     return distance
